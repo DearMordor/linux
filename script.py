9cad87ae07cc47de474654af5db3f89baab78309
dearmordor <40562491+DearMordor@users.noreply.github.com>
2023-12-24 21:06:26 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import re
+import glob
 from collections import Counter
 
 time_now = datetime.now()
@@ -8,24 +9,23 @@ failed_attempts = Counter()
 
 failed_ssh_attempt_re = re.compile(r'Failed password for .* from (\S+) port \d+ ssh2')
 
-current_year = time_now.year
+for filename in glob.glob('/root/uop/log/auth.log*'):
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'Failed password' not in line:
+                continue
 
-with open('/root/uop/log/auth.log', 'r') as file:
-    for line in file:
-        if 'Failed password' not in line:
-            continue
+            month, day, time_of_day, _, _, _, _ = line.split()[:7]
+            log_time_str = f"{month} {day} {time_of_day}"
+            log_time = datetime.strptime(log_time_str, '%b %d %H:%M:%S')
 
-        month, day, time_of_day, _, _, _, _ = line.split()[:7]
-        log_time_str = f"{month} {day} {time_of_day}"
-        log_time = datetime.strptime(log_time_str, '%b %d %H:%M:%S')
+            log_time = log_time.replace(year=time_now.year)
 
-        log_time = log_time.replace(year=time_now.year)
-
-        if time_24hrs_ago <= log_time <= time_now:
-            failed_match = failed_ssh_attempt_re.search(line)
-            if failed_match:
-                ip_address = failed_match.group(1)
-                failed_attempts[ip_address] += 1
+            if time_24hrs_ago <= log_time <= time_now:
+                failed_match = failed_ssh_attempt_re.search(line)
+                if failed_match:
+                    ip_address = failed_match.group(1)
+                    failed_attempts[ip_address] += 1
 
 with open('/tmp/stat.txt', 'w') as out_file:
     for ip, count in failed_attempts.most_common():
