6245f01d0b6e168706e89a3445cd310ce78a3c44
dearmordor <40562491+DearMordor@users.noreply.github.com>
2023-12-24 21:04:10 +0100

    Update script.py

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ with open('/root/uop/log/auth.log', 'r') as file:
         if 'Failed password' not in line:
             continue
 
-        month, day, time_of_day, _, _, _, _ = line.split()[:8]
+        month, day, time_of_day, _, _, _, _ = line.split()[:7]
         log_time_str = f"{month} {day} {time_of_day}"
         log_time = datetime.strptime(log_time_str, '%b %d %H:%M:%S')
 
