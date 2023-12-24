a1d896ff0b8322475136ff85a3bffbef44f2dfcc
dearmordor <40562491+DearMordor@users.noreply.github.com>
2023-12-24 21:03:47 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,15 +2,12 @@ from datetime import datetime, timedelta
 import re
 from collections import Counter
 
-# Set the time range from now to 24 hours ago
 time_now = datetime.now()
 time_24hrs_ago = time_now - timedelta(days=1)
 failed_attempts = Counter()
 
-# Regular expression for matching failed SSH attempts and extracting IP
 failed_ssh_attempt_re = re.compile(r'Failed password for .* from (\S+) port \d+ ssh2')
 
-# Assume logs are from the current year for simplicity; adjust as needed
 current_year = time_now.year
 
 with open('/root/uop/log/auth.log', 'r') as file:
@@ -30,7 +27,6 @@ with open('/root/uop/log/auth.log', 'r') as file:
                 ip_address = failed_match.group(1)
                 failed_attempts[ip_address] += 1
 
-# Write the statistics to a file
 with open('/tmp/stat.txt', 'w') as out_file:
     for ip, count in failed_attempts.most_common():
         out_file.write(f"{ip}: {count}\n")
