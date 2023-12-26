adb40f1b4b88b7ae4696e3354bb2eaff9ff9a4f1
dearmordor <40562491+DearMordor@users.noreply.github.com>
2023-12-26 21:26:07 +0100

    implement error handling | update path to python script script.sh

--- script.sh
+++ script.sh
@@ -4,9 +4,20 @@ remote_user="user"
 remote_host="host"
 remote_port="8822"
 remote_log_path="/var/log/auth.log*"
-local_log_path="/root/uop/log"
+uop_path="/root/uop"
+local_log_path="$uop_path/log"
 web_root="/var/www/html"
 
 scp -P $remote_port $remote_user@$remote_host:$remote_log_path $local_log_path
 
-python3 script.py
+if [ $? -ne 0 ]; then
+    echo "Error in transferring log files."
+    exit 1
+fi
+
+python3 "$uop_path/script.py"
+
+if [ $? -ne 0 ]; then
+    echo "Error in executing Python script."
+    exit 1
+fi
